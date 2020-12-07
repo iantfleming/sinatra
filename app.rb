8bc9d941c5215b119b323d6cd44c4dec471e2ec7
Ian Fleming <>
2020-12-07 17:31:19 +0000

    query string

--- app.rb
+++ app.rb
@@ -13,7 +13,13 @@ get '/chris' do
   'Chris\' world!'
 end
 
-get '/cat' do
-  @random_name = ["Amigo", "Oscar", "Viking"].sample
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
